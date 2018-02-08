81d637dcc9314d7f13fb5bb49d168dda672ab63f
Jon <rfishkin1313@gmail.com>
2018-02-08 13:00:44 -0500

    Update

--- Lecture3/s07_Hw2b/src/s07_hw2b/S07_Hw2b.java
+++ Lecture3/s07_Hw2b/src/s07_hw2b/S07_Hw2b.java
@@ -10,19 +10,8 @@ package s07_hw2b;
  * @author jojob
  */
 public class S07_Hw2b {
-
-    private double n;
     
-    
-    public S07_Hw2b(double x){
-    
-    n = x;
-    
-}
-
-    
-    
-    public double forward(){
+    public static double forward(double n){
         double sum = 0;
         for(double i = 1; i <= n; i++ ){
             double eq = i * i;
@@ -32,11 +21,35 @@ public class S07_Hw2b {
        return sum; 
     }
     
+    public static double backward(double n){
+        double sum = 0;
+        for (double i = n; i > 0; i--){
+            double eq = i * i;
+            sum = sum + 1 / eq;
+        }
+       return sum;
+    }
+    
     
     
     public static void main(String[] args){
        
-        double y = forward(1);
+   
+        System.out.println(forward(1));
+        System.out.println(forward(10));
+        System.out.println(forward(100));
+        System.out.println(forward(1000));
+        System.out.println(forward(10000));
+        System.out.println(forward(100000));
+        
+        System.out.println("Below is backwards calculation");
+        
+        System.out.println(backward(1));
+        System.out.println(backward(10));
+        System.out.println(backward(100));
+        System.out.println(backward(1000));
+        System.out.println(backward(10000));
+        System.out.println(backward(100000));
         
         
     }
