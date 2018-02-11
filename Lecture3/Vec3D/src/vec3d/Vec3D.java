d126bb14cb34d67814f09eec52be53effde953db
Jonathan <rfishkin1313@gmail.com>
2018-02-11 17:00:09 -0500

    Update

--- Lecture3/Vec3D/src/vec3d/Vec3D.java
+++ Lecture3/Vec3D/src/vec3d/Vec3D.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package vec3d;
-
 /**
  *
- * @author jojob
+ * Author : Jonathan Fishkin
+ * Cite: Suhail Mansuri
  */
+
+package vec3d;
+
 public class Vec3D {
 
     double x , y, z;
